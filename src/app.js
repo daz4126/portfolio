f4846f90308ca6f0716028fc7c48646c99734725
daz4126 <daz4126@gmail.com>
2024-03-20 16:41:57 +0000

    colour updates

--- src/app.js
+++ src/app.js
@@ -8,19 +8,19 @@ const app = Koala()
 // components
 const Header = props => html`
 <header>
-  <h1 class="text-center lightGrey-text italic">Hey! 👋 I'm <b class="lightPurple-text">Daz</b></h1>
-  <h2 class="display-2 text-center grey-text">I make stuff for the web ...</h2>
+  <h1 class="text-center lightGrey-text italic brand">Hey! 👋 I'm <b class="neonPink-text glow">Daz</b></h1>
+  <h2 class="display-2 text-center gradient-text padding">I make stuff for the web ...</h2>
 
   <nav class="nav align-center">
-  <button class="blue"><i class="fa-brands fa-github"></i> GitHub</button>
-  <button class="blue"><i class="fa-brands fa-linkedin"></i> LinkedIn</button>
+  <button class="neonBlue"><i class="fa-brands fa-github"></i> GitHub</button>
+  <button class="neonBlue"><i class="fa-brands fa-linkedin"></i> LinkedIn</button>
   </nav>
   <p class="padding text-center bold italic">I'm currently available for work. Get in touch if you see something you like!</p>
 </header>`
 
 const Gallery = props => html`
 <main id="gallery">
-  <h2 class="display-3 grey-text">Here's  some stuff I've made ...</h2>
+  <h2 class="display-3 gradient-text padding">Here's  some stuff I've made ...</h2>
   <ul class="no-bullet grid">
     ${projects.map(project => html`<${Project} ...${project}/>`)}
   </ul>
@@ -28,7 +28,7 @@ const Gallery = props => html`
 
 const Project = props => html`
   <li class="card">
-    <header><h3 class="text-center">${props.title}</h3></header>
+    <header><h3 class="text-center lightGrey-text">${props.title}</h3></header>
     <main>
       <img src="${props.image}" class=""/>
       <p>${props.description}</p>
