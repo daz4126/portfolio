c209e3a4b1e4609319ccd20f164d53ad1bca6d38
daz4126 <daz4126@gmail.com>
2024-03-27 15:35:53 +0000

    Separated skills and achievments

--- src/skills.js
+++ src/skills.js
@@ -1,14 +1,18 @@
 const skills = [
-    "I can code in JavaScript, TypeScript, Haskell, Ruby and Python",
-    "I've written 2 books about JavaScript (see below)",
-    "I can write semantic HTML and CSS",
-    "I've written my own CSS framework, that's a leaner alternative to Tailwind",
-    "I have used frontend frameworks such as React, Vue and Svelte",
-    "I've written my own state management library that is a smaller and faster alternative to React",
-    "I have used backend frameworks such as Express, Ruby on Rails, Flask and Sinatra",
-    "I've written a book about Sinatra (see below)",
-    "I've written lots of articles for SitePoint",
-    "And a few for Scrimba"
+    "Can code in JavaScript, TypeScript, Haskell, Ruby and Python",
+    "Can write semantic HTML and CSS",
+    "Can confidently use frontend frameworks such as React, Vue and Svelte",
+    "Confident with backend frameworks such as Express, Ruby on Rails, Flask and Sinatra",
+    "Can use GraphQL and other database technologies"
   ]
 
-  export { skills }
+  const achievements = [
+    "2 published books about JavaScript (see below)",
+    "Created my own CSS framework, that's a leaner alternative to Tailwind",
+    "Created my own state management library that is a smaller and faster alternative to React",
+    "Written a book about Sinatra (see below)",
+    "Lots of articles published bu SitePoint",
+    "A few articles published by Scrimba"
+  ]
+
+  export { skills, achievements }
